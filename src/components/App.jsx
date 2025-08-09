dfc06180dc8986ae49b6a2bf6633f3e4a244df0b
naleilan <leilanschmitz@gmail.com>
2025-08-09 09:42:42 +0330

    added readme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-function App() {
-  return <div></div>;
-}
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <p>Based on Leetcode challenges</p>
+      <p>Contents</p>
+
+      <p>
+        <b>Day 1</b> - [2619. Array Prototype
+        Last](https://github.com/LinSchmitz/100-Days/tree/main/Day%201)
+      </p>
+      <p>Please ⭐ this repository if you loved it !</p>
+    </div>
+  );
+}
